addfc4c96d85a6e0f5ba42b8e97da2836fcc3f6c
santiCM <santiago.canas@campus.xaloc.org>
2024-09-13 12:29:53 +0200

    Add files via upload

---
+++ programacion/python/ejemplos/ejemplo_var1.py
@@ -0,0 +1,13 @@
+
+#se trabajara con 3 tipos de variable
+#en python el tipo de variable lo define el valor de la propia variable
+
+var1=3
+var_num=3.5
+var_txt="hola"
+
+print(var1)
+
+var1="ole"
+
+print(var1)
---
+++ programacion/python/ejemplos/ejemplo_var2.py
@@ -0,0 +1,9 @@
+#2 maneras de trabajar con print
+
+var1=3
+var_num=3.5
+var_txt="hola"
+
+
+#texto entre comillas separados por comas
+print("el total es", var1,"y el cambio es", var_num)
