aff721fcaf70c64e13e37ededa92ec8580dad83d
santiCM <santiago.canas@campus.xaloc.org>
2024-09-27 12:39:13 +0200

    Update ejercicio9.py

--- programacion/python/ejercicios_iniciales/ejercicio9.py
+++ programacion/python/ejercicios_iniciales/ejercicio9.py
@@ -1,1 +1,5 @@
-secs=int(input("introduce un numero de segundos: "
+secs=int(input("introduce un numero de segundos: "))
+mins=secs/60
+horas=mins/60
+horas=round(horas, 2)
+print(f"el número de minutos es: {mins} y en horas es: {horas}")
