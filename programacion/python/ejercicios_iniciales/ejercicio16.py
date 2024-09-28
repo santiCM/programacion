8ea455c355e3cf164078816e2adefc5102600fda
santiCM <santiago.canas@campus.xaloc.org>
2024-09-29 01:05:17 +0200

    Update ejercicio16.py

--- programacion/python/ejercicios_iniciales/ejercicio16.py
+++ programacion/python/ejercicios_iniciales/ejercicio16.py
@@ -1,1 +1,8 @@
 import math
+var1=int(input("introduce un numero: "))
+raiz=(math.sqrt(var1))
+raiz=round(raiz,1)
+divi=raiz/2
+divi=(math.floor(divi))
+print("la raiz es:", raiz)
+print("la division es:", divi)
