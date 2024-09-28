99fd5ff8f83c2eaeda8fcb3017e0ea7a012f5127
santiCM <santiago.canas@campus.xaloc.org>
2024-09-29 00:52:11 +0200

    ejercicio 15

--- programacion/python/ejercicios_iniciales/ejercicio15.py
+++ programacion/python/ejercicios_iniciales/ejercicio15.py
@@ -1,1 +1,9 @@
 import math
+rad=int(input("dame un numero:" ))
+hi=int(input("dame otro:" ))
+vol=(math.pi)*(rad**2)*hi
+area=(2*(math.pi)*rad*hi)+(2*(math.pi)*(rad**2))
+vol=round(vol,2)
+area=round(area,2)
+print("el area del cilindro es", area)
+print("el volumen del cilindro es", vol)
