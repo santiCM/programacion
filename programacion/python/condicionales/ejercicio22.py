65846839044632f7bff74b638e2697fe1d03b57b
santiCM <santiago.canas@campus.xaloc.org>
2024-10-18 12:12:27 +0200

    Update ejercicio22.py

--- programacion/python/condicionales/ejercicio22.py
+++ programacion/python/condicionales/ejercicio22.py
@@ -1,3 +1,4 @@
+# Programa que al introducir una nota por teclado te diga si has aprobado o suspendido. Si la nota es menos de un 5 es suspenso y si la nota es 5 o mayor estás aprobado.
 nota=float(input("Introduce la nota "))
 
 if nota>10 or nota<0:
