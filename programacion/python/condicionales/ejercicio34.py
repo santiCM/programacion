e0e0149fffbbf66be5e409f48cb1dbbbbe549c60
santiCM <santiago.canas@campus.xaloc.org>
2024-10-24 19:56:49 +0200

    Add files via upload

---
+++ programacion/python/condicionales/ejercicio34.py
@@ -0,0 +1,16 @@
+
+var_numero="6734"
+var_suma=0
+
+var_longitud=len(var_numero)
+var0=int(var_numero[0])
+var1=int(var_numero[1])
+var2=int(var_numero[2])
+var3=int(var_numero [3])
+
+var_suma=int(var0+var1+var2+var3 )
+
+if var_suma // 2 == 0:
+ print (f"el valor de {var_suma} es impar")
+else:
+    print(f"el valor de {var_suma} es par")
