1025ed5cdcb82774efa059748aba71c7b04eeeb6
santiCM <santiago.canas@campus.xaloc.org>
2025-02-05 13:37:07 +0100

    Update sieteymediofase3.py

--- programacion/python/sieteymediofase3.py
+++ programacion/python/sieteymediofase3.py
@@ -15,18 +15,18 @@ if carta=="s":
     if numero==10 or numero==11 or numero==12:
         jtotal+=0.5
         print(f"{nombre}, tu carta es: {numero}")
-        print("el total acumulado es: ",total)
+        print("el total acumulado es: ",jtotal)
     else:
         jtotal+=numero
         print(f"{nombre}, tu carta es: {numero}")
-        print("el total acumulado es: ",total)
+        print("el total acumulado es: ",jtotal)
 if carta=="n":
     print("AWENO")
     respuesta="n"
     repeticiones=0
 
 
-while carta=="s" or total<7.5:
+while carta=="s" and jtotal<7.5:
     respuesta="n"
     print("tus puntos: ", puntos)
     if puntos>0:
@@ -39,30 +39,35 @@ while carta=="s" or total<7.5:
     if carta=="s":
         numero=random.choice(palos)
         if numero==10 or numero==11 or numero==12:
-            total+=0.5
+            jtotal+=0.5
             print(f"{nombre}, tu carta es: {numero}")
             print("{nombre}, el total acumulado es: ",jtotal)
         else:
-            total+=numero
+            jtotal+=numero
             print(f"{nombre}, tu carta es: {numero}")
             print("{nombre}, el total acumulado es: ",jtotal)
-        if total>7.5:
+        if jtotal>7.5:
+            print("Te has pasado")
             Banca=True
             puntos-=10
             carta="n"
        
     if carta=="n":
-        if total<6:
+        if jtotal<6:
             print("Quizás tendrías que arriesgar un poco ¿no?")
-            puntos-=5           
-        if total>=6 and total<=7:
+            puntos-=5  
+            Banca=True
+        if jtotal>=6 and jtotal<=7:
             print("Has sido un poco conservador")
-            puntos+=5        
+            Banca=True
+            puntos+=5
     if carta!="s" and carta!="n" :
         print("esa respuesta no es valida")
-   
 else:
+    carta="s"
     while Banca==True:
+        if carta=="n" :
+            Banca=False
         if carta=="s":    
             numero=random.choice(palos)
             if numero==10 or numero==11 or numero==12:
@@ -84,8 +89,27 @@ else:
                     print("Gana la banca")
             if btotal>=7 and btotal<=7.5:
                 carta="n"
-                print("{nombre}, el total acumulado es: ",jtotal)
-                print("el total acumulado por la banca es: ",btotal)
                 
-                    
+            if btotal>7.5 :
+                carta="n"
+                Banca=False
+            
+    else:
+        print("{nombre}, el total acumulado es: ",jtotal)
+        print("el total acumulado por la banca es: ",btotal)  
+        Banca=False
+        if jtotal==7.5 and btotal!=7.5:
+            print(f"Gana {nombre}")
+        if btotal==7.5 and jtotal!=7.5:
+            print("Gana la banca")
+        if jtotal>=7.5 and btotal<jtotal :
+            print(f"Gana {nombre}")
+        if jtotal>=7.5 and btotal>7.5 :
+            print(f"Gana {nombre}")
+        if btotal>=7.5 and jtotal<btotal :
+            print(f"Gana la banca")
+        if btotal>=7.5 and jtotal>7.5 :
+            print(f"Gana la banca")
+        if btotal<7.5 and jtotal<7.5 and btotal>jtotal:
+            print("Gana la banca")
     
